2895cac2152de434787eb35f86979f87f10ad9c6
DarkShark02 <flavio.ruggiero27@gmail.com>
2022-12-08 19:37:35 +0100

    ciao

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,29 +1,74 @@
-from flask import Flask
+import os
+import pathlib
 import secrets
+
+import google.auth.transport.requests
+from flask import Flask, session, abort, redirect, request
+from google_auth_oauthlib.flow import Flow
 secret_key = secrets.token_hex(16)
 app = Flask("FrontlineCode")
-# example output, secret_key = 000d88cd9d90036ebdd237eb6b0db000
 app.config['SECRET_KEY'] = secret_key
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 #app.secret.key="CodeSpecialist.com"
+GOOGLE_CLIENT_ID="412052900468-fmptddpq7os5ld3u9bbjpuvm6j35mf6q.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent,"client-secret.json")
+
+flow= Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email",
+            "openid"],
+    redirect_uri="https://127.0.0.1:5000/callback"
+)
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            abort(401) #Authorization required
+        else:
+            return function()
+    return wrapper
+
+
 @app.route("/login")
 def login():
-    pass
+    authorization_url, state =flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
 @app.route("/callback")
 def callback():
-    pass
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500) #States does not match
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    return id_info
+
+
+
 
 @app.route("/logout")
 def logout():
-    pass
+    session.clear()
+    return redirect("/")
 
 @app.route("/")
 def index():
-    return "Mattia"
+    return "index <a href='/login'><button>Login</button></a>"
 
 @app.route("/protected_area")
+@login_is_required
 def protected_area():
-    pass
+    return "protected_area <a href='/logout'><button>Logout</button></a>"
 
 if __name__ == "__main__":
     app.run(debug=True)
