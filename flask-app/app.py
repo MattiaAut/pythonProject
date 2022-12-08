7ac3768937d80b095268ef725ef040ed903d4d38
DarkShark02 <flavio.ruggiero27@gmail.com>
2022-12-08 20:59:55 +0100

    ciao

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,13 +1,13 @@
+import requests
 import os
 import pathlib
 import secrets
-
-import cachecontrol as cachecontrol
-import google.auth.transport.requests
 from flask import Flask, session, abort, redirect, request
-from google.auth.transport import requests
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
 secret_key = secrets.token_hex(16)
 app = Flask("FrontlineCode")
 app.config['SECRET_KEY'] = secret_key
@@ -20,7 +20,7 @@ flow= Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email",
             "openid"],
-    redirect_uri="https://127.0.0.1:5000/callback"
+    redirect_uri="http://127.0.0.1:5000/callback"
 )
 def login_is_required(function):
     def wrapper(*args, **kwargs):
@@ -42,7 +42,7 @@ def callback():
     flow.fetch_token(authorization_response=request.url)
 
     if not session["state"] == request.args["state"]:
-        abort(500) #States does not match
+        abort(500)  # State does not match!
 
     credentials = flow.credentials
     request_session = requests.session()
@@ -54,12 +54,10 @@ def callback():
         request=token_request,
         audience=GOOGLE_CLIENT_ID
     )
+
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
     return redirect("/protected_area")
-
-
-
 
 @app.route("/logout")
 def logout():
