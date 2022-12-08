403810731cb225f8ac6e328982c2f45111054c70
DarkShark02 <flavio.ruggiero27@gmail.com>
2022-12-08 18:28:14 +0100

    ciao

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template
-from flask_login import LoginManager
-login_manager = LoginManager()
-login_manager.init_app(app)
-app=Flask(__name__)
-@app.route('/')
+from flask import Flask
+import secrets
+secret_key = secrets.token_hex(16)
+app = Flask("FrontlineCode")
+# example output, secret_key = 000d88cd9d90036ebdd237eb6b0db000
+app.config['SECRET_KEY'] = secret_key
+#app.secret.key="CodeSpecialist.com"
+@app.route("/login")
+def login():
+    pass
+
+@app.route("/callback")
+def callback():
+    pass
+
+@app.route("/logout")
+def logout():
+    pass
+
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return "Mattia"
+
+@app.route("/protected_area")
+def protected_area():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
